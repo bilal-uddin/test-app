346a93cbd76aa25c1eebf672d4a5452258c40df2
bilal-uddin <bilaluddinwardak@gmail.com>
2023-05-06 13:25:57 +0200

    first tree trail

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React is very easy to learn
         </a>
       </header>
     </div>
