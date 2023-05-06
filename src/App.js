97800f3039c63d57bedbadf65d6cdf93f639cdb4
bilal-uddin <bilaluddinwardak@gmail.com>
2023-05-06 13:37:21 +0200

    2nd changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmad
+          Learn React with ahmad 
+          bilal uddin
         </a>
       </header>
     </div>
